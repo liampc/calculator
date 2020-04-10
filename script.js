d4fb682cd3ea96832205f8a38f6409283cb602d2
Kathlia Carlos <lia.carlos23@gmail.com>
2020-04-10 13:47:07 +0800

    make numBtns appear on equationsBox

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const answerBox = document.querySelector("#answerBox");
 const equationsBox = document.querySelector("#equationsBox");
 
 
+const numBtns = document.querySelectorAll(".num");
+
+numBtns.forEach((button) => {
+    addEventListener('click', getNum)
+})
+
+function getNum(){
+    if (event.target.className == "num"){
+        let val = event.target.textContent;
+        equationsBox.innerHTML += val;
+    }
+}
+
+
