478c367459316c445af184c18fb4a153569db200
Kathlia Carlos <lia.carlos23@gmail.com>
2020-04-15 09:24:07 +0800

    list down selectors

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-const answerBox = document.querySelector("#answerBox");
-const equationsBox = document.querySelector("#equationsBox");
-
-
+const answerBox = document.querySelector('#answerBox');
+const equationsBox = document.querySelector('#equationsBox');
 const numBtns = document.querySelectorAll(".num");
-numBtns.forEach((button) => {
-    addEventListener('click', clickNum)
-})
-
-function clickNum(){
-    if (event.target.className == "num"){
-        let val = event.target.textContent;
-        equationsBox.innerHTML += val;
-    }
-}
-
-
+const operationBtns = document.querySelectorAll(".operations");
+const clearBtn = document.querySelector('#clear');
+const delBtn = document.querySelector('#delete');
