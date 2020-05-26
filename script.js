1891e9878ccbdd1d0e808b92e48a55fa3b3a1b02
Kathlia Carlos <lia.carlos23@gmail.com>
2020-05-26 20:51:01 +0800

    redo js for event delegation pattern

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-const answerBox = document.querySelector('#answerBox');
-const equationsBox = document.querySelector('#equationsBox');
-const numBtns = document.querySelectorAll(".num");
-const operationBtns = document.querySelectorAll(".operatorsBtns");
-const clearBtn = document.querySelector('#clear');
-const delBtn = document.querySelector('#delete');
+
 
 
 
