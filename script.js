af244b0ad5232cfe92dc8a747f5d2044768380a0
Kathlia Carlos <lia.carlos23@gmail.com>
2020-05-27 09:52:05 +0800

    add base event delegation

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
-const calculator = document.querySelector("#calCont");
-const btns = calculator.querySelector("#btnsDiv")
+const calculator = document.querySelector('.calcCont');
+const btns = calculator.querySelector('.btnsDiv');
+const display = calculator.querySelector('.displayBox')
 
+btns.addEventListener('click', e => {
+    if (e.target.matches('button')){
+       const btn = e.target;
+       const operation = btn.dataset.operation;
+       const btnName = btn.textContent;
+       const displayValue = display.textContent;
 
+       if (!operation){ // for number btns
+          console.log('its a number')
+       }
+
+       if ( operation === 'multiply' ||
+            operation === 'divide' ||
+            operation === 'add' ||
+            operation === 'subtract'){
+           console.log('its an operator')
+       }
+
+       if (operation === 'decimal'){
+        console.log('its a decimal')
+       }
+
+       if (operation === 'calculate'){
+        console.log('its equals')
+       }
+
+       if (operation === 'clear'){
+        console.log('its clear')
+       }
+
+       if (operation === 'delete'){
+        console.log('its backspace')
+       }
+       
+    }
+
+})
 
 
